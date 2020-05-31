e5c41358cc7eabcd672a597e151b6da8aeb629c3
MukendiMputu <mputusam@live.com>
2020-05-31 22:12:11 +0200

    finished main exercises

--- A2/aufgabe2_a.c
+++ A2/aufgabe2_a.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <errno.h>
 #include <stdlib.h>
@@ -26,6 +27,7 @@ struct thread_info {    /* Used as argument to thread_start() */
    int       thread_num;       /* Application-defined thread # */
 };
 
+
 // Routine, die von den jeweiligen Kassen (Thread) ausgeführen werden
 // Da c default-mäßig pass-by-value ist, muss die Referenz übergeben werden
 static void *
@@ -33,9 +35,12 @@ bezahlvorgang(void *param_anzahl_kunden)
 {
   int *anzahl_kunden = param_anzahl_kunden;
 
-  while ( anzahl_kunden > 0 ) {
-    // Kunden abrechnen und
-    // rausbitten
+  while ( *anzahl_kunden > 0 ) {
+    // Kunden abrechnen und rausbitten
+    printf("Bearbeite Kunden \e[32m%d\e[m ab...\n", *anzahl_kunden);
+    *anzahl_kunden -= 1;
+    sleep(SLEEP_TIME);
+    printf("Kunde abgearbeitet und raus.\n");
   }
 
   return anzahl_kunden;
@@ -47,11 +52,19 @@ int main(int argc, char const *argv[])
   struct thread_info *tinfo;
   pthread_attr_t attr;
 
+  /* Initialize thread creation attributes */
+  status = pthread_attr_init(&attr);
+  if (status != 0)
+     handle_error_en(status, "pthread_attr_init");
+
+
   // alloziiert Speicherplatz für NUM_THREADS Threads von je sizeof
   // (struct thread_info) Bytes
   tinfo = calloc(NUM_THREADS, sizeof(struct thread_info));
   if (tinfo == NULL)
     handle_error("calloc");
+
+  printf("\nKunden im Supermarkt: \e[32m%d\e[m.\n\n", shared_kunden_anzahl);
 
    /* Für je eine Kasse ein Thread wird erzeugt */
    for (int tnum = 0; tnum < NUM_THREADS; tnum++) {
@@ -65,6 +78,20 @@ int main(int argc, char const *argv[])
            handle_error_en(status, "pthread_create");
    }
 
+    /* Destroy the thread attributes object */
+    status = pthread_attr_destroy(&attr);
+    if (status != 0)
+       handle_error_en(status, "pthread_attr_destroy");
+
+   /* warte bis alle Threads beendet sind*/
+   for (int tnum = 0; tnum < NUM_THREADS; tnum++) {
+     status = pthread_join(tinfo[tnum].thread_id, NULL);
+       if (status != 0)
+          handle_error_en(status, "pthread_create");
+     }
+
+
+  printf("\n\e[32m%d\e[m Kunden sind noch im Supermarkt.\n", shared_kunden_anzahl);
 
   free(tinfo);
   return 0;
--- A2/aufgabe2_c.c
+++ A2/aufgabe2_c.c
@@ -1,1 +1,108 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <errno.h>
+#include <stdlib.h>
+
+/*
+ * Mukendi Mputu , Tobias Weimann , Ahmad Siar Latifi
+ */
+
+
+#define NUM_THREADS 5
+#define SLEEP_TIME 3
+
+
+// Allgemeine Fehlerbehandlung der Systemaufrufe
+#define handle_error(msg) \
+              do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+// Fehlerbehandlung der Systemaufrufe mit Statuscode
+#define handle_error_en(en, msg) \
+              do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+
+// Datenstruktur für die Thread Infos
+struct thread_info {    /* Used as argument to thread_start() */
+   pthread_t thread_id;        /* ID returned by pthread_create() */
+   int       thread_num;       /* Application-defined thread # */
+};
+
+pthread_mutex_t lock;
+
+// Routine, die von den jeweiligen Kassen (Thread) ausgeführen werden
+// Da c default-mäßig pass-by-value ist, muss die Referenz übergeben werden
+static void *
+bezahlvorgang(void *param_anzahl_kunden)
+{
+  int *anzahl_kunden = param_anzahl_kunden;
+
+  while ( *anzahl_kunden > 0 ) {
+    // Kunden abrechnen und rausbitten
+    if(pthread_mutex_lock(&lock))
+      handle_error("mutex lock error");
+
+    if (*anzahl_kunden > 0){
+      printf("Bearbeite Kunden \e[32m%d\e[m ab...\n", *anzahl_kunden);
+      sleep(SLEEP_TIME);
+      *anzahl_kunden -= 1;
+      printf("Kunde abgearbeitet und raus.\n");
+    }
+
+    if(pthread_mutex_unlock(&lock))
+      handle_error("mutex unlock error");
+
+  }
+
+  return anzahl_kunden;
+}
+
+int main(int argc, char const *argv[])
+{
+  int shared_kunden_anzahl = 42, status;
+  struct thread_info *tinfo;
+  pthread_attr_t attr;
+
+  /* Initialize thread creation attributes */
+  status = pthread_attr_init(&attr);
+  if (status != 0)
+     handle_error_en(status, "pthread_attr_init");
+
+
+  // alloziiert Speicherplatz für NUM_THREADS Threads von je sizeof
+  // (struct thread_info) Bytes
+  tinfo = calloc(NUM_THREADS, sizeof(struct thread_info));
+  if (tinfo == NULL)
+    handle_error("calloc");
+
+  printf("\nKunden im Supermarkt: \e[32m%d\e[m.\n\n", shared_kunden_anzahl);
+
+   /* Für je eine Kasse ein Thread wird erzeugt */
+   for (int tnum = 0; tnum < NUM_THREADS; tnum++) {
+       tinfo[tnum].thread_num = tnum + 1;
+
+       // pthread_create() speichert die Thread-ID in die entspreched Variable
+       // tinfo[tnum].thread_id
+       status = pthread_create(&tinfo[tnum].thread_id, &attr,
+                          &bezahlvorgang, &shared_kunden_anzahl);
+       if (status != 0)
+           handle_error_en(status, "pthread_create");
+   }
+
+    /* Destroy the thread attributes object */
+    status = pthread_attr_destroy(&attr);
+    if (status != 0)
+       handle_error_en(status, "pthread_attr_destroy");
+
+   /* warte bis alle Threads beendet sind*/
+   for (int tnum = 0; tnum < NUM_THREADS; tnum++) {
+     status = pthread_join(tinfo[tnum].thread_id, NULL);
+       if (status != 0)
+          handle_error_en(status, "pthread_create");
+     }
+
+
+  printf("\n\e[32m%d\e[m Kunden sind noch im Supermarkt.\n", shared_kunden_anzahl);
+  pthread_mutex_destroy(&lock);
+  free(tinfo);
+  return 0;
+}
