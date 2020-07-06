5d80da83a362c6781783f9bd8bf7c4f55cd42a4a
MukendiMputu <mputusam@live.com>
2020-07-06 18:14:27 +0200

    downloaded vorgabe.tar.gz

--- A4/4a.c
+++ A4/4a.c
@@ -1,1 +1,30 @@
- 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "4a.h"
+
+
+ struct mem_info {
+   int length;
+   char status;
+ } feld [64];
+
+void *nf_alloc(size_t size)
+{
+	/*
+	 * Diese Funktion sollt ihr implementieren.
+	 */
+
+
+	/*
+	 * Der Laufindex
+	 */
+	 size_t chunk_index;
+
+
+
+	/*
+	 * Gibt die Startadresse des belegten Speicher zurück.
+	 */
+	return heap + chunk_index * CHUNK_SIZE;
+}
--- A4/4b.c
+++ A4/4b.c
@@ -1,1 +1,13 @@
- 
+#include "4b.h"
+#include <stdio.h>
+
+void nf_free(void *addr)
+{
+	// Diese Funktion sollt ihr implementieren
+
+
+	/*
+	 * chunk_index ist der Index des Chunks, in dem addr liegt.
+	 */
+	size_t chunk_index = ((char*)addr - heap) / CHUNK_SIZE;
+}
