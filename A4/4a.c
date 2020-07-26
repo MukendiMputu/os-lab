a01fc12fa3c5262e8bd1a0b179675ad3be96d430
MukendiMputu <mputusam@live.com>
2020-07-26 20:06:25 +0200

    exercises 4

--- A4/4a.c
+++ A4/4a.c
@@ -4,27 +4,44 @@
 #include "4a.h"
 
 
- struct mem_info {
-   int length;
-   char status;
- } feld [64];
+char yet_allocated = 0;
 
 void *nf_alloc(size_t size)
 {
-	/*
-	 * Diese Funktion sollt ihr implementieren.
-	 */
+  // Der index soll ueber mehrere Funktionsaufrufe erhalten bleiben
+  static size_t chunk_index = 0;
+  size_t i = 0;
+   if ( size ) {
+     for (i = chunk_index; i <= NUM_CHUNKS; i++) {
+       // if ((i == NUM_CHUNKS) && !yet_allocated)
+       //   i = 0;
 
+       // Wenn eine Lücke entdeckt wird...
+       if (allocation_list[i].status == CHUNK_FREE) {
 
-	/*
-	 * Der Laufindex
-	 */
-	 size_t chunk_index;
+         // und wenn sie gross genug ist, beleg diese und aktualisiere status
+          if ( size_to_chunks(size) <= allocation_list[i].length ) {
+            allocation_list[i].status = CHUNK_ALLOCATED;
+            allocation_list[i].length = size_to_chunks(size);
+            //yet_allocated = 1;
+            chunk_index = i;
+            break;
+          }
 
+        } else if (allocation_list[i].status == CHUNK_ALLOCATED) {
+            i += allocation_list[i].length;
+            // if (i == chunk_index) {
+            //
+            // }
+        }
+     }
 
+  	/*
+  	 * Gibt die Startadresse des belegten Speicher zurück.
+  	 */
+  	return heap + chunk_index * CHUNK_SIZE;
+  } else {
+    return NULL;
+  }
 
-	/*
-	 * Gibt die Startadresse des belegten Speicher zurück.
-	 */
-	return heap + chunk_index * CHUNK_SIZE;
 }
